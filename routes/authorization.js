94508769508b9d54cfd91057dcd96c9f4782957c
atLAhs2000 <alyssalahoy@gmail.com>
2022-10-28 22:47:05 -0700

    create authorization/login routes

---
+++ routes/authorization.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    res.send('login endpoint');
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/favorites', require('./favorites'));
 router.use('/api-docs', require('./docs'));
+router.use('/authorization', require('./authorization'))
 
 module.exports = router;
